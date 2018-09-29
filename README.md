449b02dacc7716164c92879bdb4d1e52ff3f389c
sbhangal <sbhangal3@gmail.com>
2018-09-29 15:32:31 -0700

    Fixed bug, updated API identifier, and added in functionality to display a box around all detected faces

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # face-recognition-api-react-udemy
+
+##Purpose
+This project is the server code for a website built to accept images and detect faces in them. The server code supplies a REST API functionality.
+
+##Deployed At
+https://face-recognition-with-react.herokuapp.com/
+
+##Built With
+Node.js
+JavaScript
+Clarifai API
+Visual Studio Code
+
+##Tested With
+Postman
--- controllers/image.js
+++ controllers/image.js
@@ -6,13 +6,12 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
     app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
         .then(data => {
             res.json(data);
         })
         .catch(err => res.status(400).json('unable to work with API'))
 }
-
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
